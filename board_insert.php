4243ceb50003fe8136d6542617b6bd7ebd0906d3
developerkimtakgu <developerkimtakgu@gmail.com>
2023-09-20 10:24:34 +0900

    수정사항 - 게시판 테스트

---
+++ board_insert.php
@@ -0,0 +1,34 @@
+<?php
+    $conn = mysqli_connect("127.0.0.1", 'root', 'dltkdghrytnsla1@', 'test');
+
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+        $enterAA = nl2br(addslashes($_POST['aa']));
+        //$enterAA = htmlspecialchars($_POST['aa']);
+
+        $result = mysqli_query($conn, "
+            INSERT INTO `board_test` 
+            SET `aa` = '{$enterAA}'
+        ");
+    }
+
+    $response = '';
+
+    if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+
+        $result = mysqli_query($conn, "SELECT * FROM `board_test` WHERE `idx` = '6'");
+
+        while ($row = mysqli_fetch_assoc($result)) {
+            //echo nl2br($row['aa']);
+
+            $response = nl2br($row['aa']);
+        }
+    }
+
+?>
+
+<form id="aa" name="bb" action="<?=$_SERVER['PHP_SELF']?>" method="POST">
+    <textarea name="aa"><?=$response?></textarea>
+
+    <input type="submit" id="btn_save" value="저장">
+</form>
