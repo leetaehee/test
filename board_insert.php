51e06d308ea131c2458deca92c1492afb0977a5f
leetaehee <lastride25@naver.com>
2024-07-21 21:12:42 +0900

    !

--- board_insert.php
+++ board_insert.php
@@ -1,8 +1,6 @@
 <?php
     $conn = mysqli_connect("127.0.0.1", 'root', 'dltkdghrytnsla1@', 'test');
 
-    // 테스트입니다..
-
     if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
         $enterAA = nl2br(addslashes($_POST['aa']));
