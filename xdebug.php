46cdaea1bce6bfc965526c08ff60740e2f3efc62
leetaehee <lastride25@naver.com>
2025-07-08 22:04:06 +0900

    echoNovi 함수 추가

--- xdebug.php
+++ xdebug.php
@@ -20,8 +20,20 @@ if ($configAdd === true) {
  * @param integer $b
  * @return integer
  */
-function add(int $a,int $b) : int {
+function add(int $a,int $b) : int 
+{
     return $a+$b;
 }
 
+function echoNovi(string $str) : string
+{
+    $rtnStr = $str;
+
+    return $rtnStr;
+}
+
 error_log("xdebug.php");
+
+// log 타입이 다양함
+// 로그 라이브러를 쓰거나 로그 종류별로 할 수 있는 메서드 만들어야함 
+error_log("echoNovi = " . echoNovi("안녕하세요"));
